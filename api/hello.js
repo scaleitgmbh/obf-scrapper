98da62e531c7506cf730d03ee7516659216a2bc0
Marko Nikolaš <36996299+markonikolas@users.noreply.github.com>
2025-05-22 00:37:25 +0200

    Revert "test vercel again"
    This reverts commit e76a14a6e5a4d9cdac90c41b09e752f874893425.
    
    Revert "vercel test"
    
    This reverts commit 0b548037fbefcada6eb1ef853805d1ccd8c18521.

--- api/hello.js
+++
@@ -1,5 +0,0 @@
-export async function GET(request) {
-	const response = await fetch('https://api.vercel.app/products');
-	const products = await response.json();
-	return Response.json(products);
-}
