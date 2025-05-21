e76a14a6e5a4d9cdac90c41b09e752f874893425
Marko Nikolaš <36996299+markonikolas@users.noreply.github.com>
2025-05-22 00:36:12 +0200

    test vercel again

---
+++ api/hello.js
@@ -0,0 +1,5 @@
+export async function GET(request) {
+	const response = await fetch('https://api.vercel.app/products');
+	const products = await response.json();
+	return Response.json(products);
+}
